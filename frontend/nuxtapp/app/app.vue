26c424b2d14e9c06fdb4f30256130ad213943f18
HY-nicochichi <PCheepon@outlook.jp>
2025-06-08 20:43:21 +0900

    Nuxt-Flask 1.0.0

---
+++ frontend/nuxtapp/app/app.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import NavBar from '~/components/NavBar.vue'
+</script>
+
+
+<template>
+  <div class="text-white">
+    <NuxtLoadingIndicator
+      color="deeppink" :height="5"
+    />
+    <NavBar/>
+    <div class="p-3">
+      <NuxtPage/>
+    </div>
+  </div>
+</template>
+
+
+<style>
+  body {background-color:rgb(0,0,50)}
+</style>
