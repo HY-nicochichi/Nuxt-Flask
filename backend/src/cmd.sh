f9098d0607a81471ab2f42565cb46d760ee1886f
HY-nicochichi <PCheepon@outlook.jp>
2026-01-06 21:14:32 +0900

    Nuxt-Flask 1.2.3

--- backend/src/cmd.sh
+++ backend/src/cmd.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-
 cd flaskapi
 python3 core.py
 exec gunicorn core:app \
   --bind 0.0.0.0:8000 \
+  --worker-tmp-dir /dev/shm \
   --worker-class gevent \
   --workers 8 \
-  --access-logfile -
+  --access-logfile - \
+  --error-logfile -
